1105d02066864b0844c7f2ea6c2a2bcf143b85fe
unknown <syahroni.ap@gmail.com>
2013-09-28 16:57:24 +0700

    commit03

---
+++ src/BiodataKu.java
@@ -0,0 +1,13 @@
+// Perintah penggunaan package versi 01
+
+package Latihan01.sesi1.bin;
+
+public class BiodataKu
+{
+public static void main(String[] data){
+System.out.println("Nama           : Syahroni Amanda Permadi");
+System.out.println("Alamat         : Bojong Gede");
+System.out.println("Golongan darah : B");
+System.out.println("Jenis Kelamin  : Pria");
+}
+}
