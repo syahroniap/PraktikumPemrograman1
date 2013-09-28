6d2b1161c16013ed2825dca3251d80a38e30a519
unknown <syahroni.ap@gmail.com>
2013-09-28 17:30:08 +0700

    commit05

---
+++ src/SyahroniSIM.java
@@ -0,0 +1,14 @@
+//Latihan Membuat Informasi SIM
+
+package Latihan01.sesi1.bin;
+
+public class SyahroniSIM
+{
+public static void main (String[] data)
+{
+System.out.println("Nama           : Syahroni Amanda Permadi");
+System.out.println("No.SIM         : 901012054371");
+System.out.println("Tipe SIM       : SIM C");
+System.out.println("Berlaku hingga : 2015");
+}
+}
