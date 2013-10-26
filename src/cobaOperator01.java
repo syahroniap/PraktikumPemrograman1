d345b094373c1bb60dbea5ed61144f64c643f1ce
unknown <syahroni.ap@gmail.com>
2013-10-26 20:42:01 +0700

    commit001

---
+++ src/cobaOperator01.java
@@ -0,0 +1,20 @@
+//perintah operator
+
+package Latihan01.sesi1.bin;
+
+public class cobaOperator01
+{
+   public static void main(String[] Xx)
+   {
+      int x=7;
+	  System.out.println("Nilai awal X adalah "+x);
+	  System.out.println("Nilai awal X adalah "+x++);
+	  System.out.println("Nilai awal X adalah "+x);
+	  System.out.println();
+	  int y=25;
+	  System.out.println("Nilai awal Y adalah "+y);
+	  System.out.println("Nilai ++Y "+y);
+	  System.out.println("Nilai Y adalah "+y);
+	}
+}
+	  
