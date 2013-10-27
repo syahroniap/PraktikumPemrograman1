7dc56b5c16c9e8d5abc354cff361a7509983a0d5
unknown <syahroni.ap@gmail.com>
2013-10-27 12:27:17 +0700

    commit001

---
+++ src/cobaOperator02.java
@@ -0,0 +1,21 @@
+//Perintah Operator
+
+package Latihan01.sesi1.bin;
+
+public class cobaOperator02
+{
+   public static void main(String[] Xx)
+   {
+      int x=2012;
+	  int kbt1 = x %4;
+	  System.out.println("Tahun : " +x);
+	  System.out.println("Kabisat = 0 / Bukan Kabisat = 1, Hasilnya "+kbt1);
+	  System.out.println();
+	  System.out.println();
+	  int y=2013;
+	  int kbt2 = y%4;
+	  System.out.println("Tahun : " +y);
+	  System.out.println("Kabisat=0 / bukan kabisat =1, Hasilnya "+kbt2);
+	  System.out.println();
+	}
+}	
