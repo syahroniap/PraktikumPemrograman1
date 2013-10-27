1adc62ab23d7702b97cd07ebe8e918f7c07631fa
unknown <syahroni.ap@gmail.com>
2013-10-27 12:34:15 +0700

    commit001

---
+++ src/icrement.java
@@ -0,0 +1,35 @@
+/*class icrement.
+ *@author (Roni)
+ *@Version (op-02)
+ */
+
+package Latihan01.sesi1.bin;
+
+public class icrement
+{
+    public static void main(String[] args)
+    {
+	    int a;
+		a=120;
+		System.out.println("contoh postfix increment : ");
+		System.out.println(a);
+		System.out.println(a++);
+		System.out.println(a);
+		a=120;
+		System.out.println("contoh prefix increment : ");
+		System.out.println(a);
+		System.out.println(++a);
+		System.out.println(a);
+		a=120;
+		System.out.println("contoh postfix decrement :");
+		System.out.println(a);
+		System.out.println(a--);
+		System.out.println(a);
+		a=120;
+		System.out.println("contoh prefix decrement : ");
+		System.out.println(a);
+		System.out.println(--a);
+		System.out.println(a);
+		
+	}
+}
