019951b9d5ee938e92ea7df8410794536ffc648c
unknown <syahroni.ap@gmail.com>
2013-11-02 18:53:31 +0700

    commit002

---
+++ src/Ulang01.java
@@ -0,0 +1,14 @@
+//Perintah control flow/struktur kontrol
+//perintah pertama - contoh 1
+
+package Latihan01.sesi1.bin;
+
+public class Ulang01
+{
+   public static void main(String[] Xx)
+   {
+      int [] daftar = {20, 30, 40};
+	  for (int x=0; x<3; x++)
+	     System.out.println("isi Elemen ke-" +x +" = "+daftar[x]);
+	}
+}
